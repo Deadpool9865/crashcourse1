83d82019e41478d2846a2f493bf6e3c542fbf325
Deadpool9865 <127920894+Deadpool9865@users.noreply.github.com>
2023-03-16 14:34:46 +0530

    Update array(product).c

--- array(product).c
+++ array(product).c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+int array[5];
+int product=1;
+printf("Enter an array\n");
+for (int i =0;i<5;i++)
+{
+    scanf("%d",&array[i]);
+}
+for (int j=0;j<5;j++){
+    product*=array[j];
+}
+printf("Products of elements=%d",product);
+}
